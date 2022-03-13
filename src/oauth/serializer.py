8b21aa7190b22d1d5223ce5ed12a991bb9d9017b
Son Of Fjord <v4lkyn@ya.ru>
2022-03-13 20:50:45 +0300

    Added the work of social links of the author

--- src/oauth/serializer.py
+++ src/oauth/serializer.py
@@ -9,6 +9,23 @@ class UserSerializer(serializers.Serializer):
         model = models.AuthUser
         fields = ('avatar', 'country', 'city', 'bio', 'display_name')
 
+
+class SocialLinkSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = models.SocialLink
+        fields = ('id', 'link')
+
+
+class AuthorSerializer(serializers.Serializer):
+    social_links = SocialLinkSerializer(many=True)
+
+    class Meta:
+        model = models.AuthUser
+        fields = ('id', 'avatar', 'country', 'city', 'bio', 'display_name', 'social_links')
+
+
 class GoogleAuth(serializers.Serializer):
     """ Сериализация данных от Google """
 
